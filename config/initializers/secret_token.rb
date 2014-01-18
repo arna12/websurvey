526abab1b1efa0f3dbbaa04d36aed357efe7d2e1
Arna12 <arna12@ru.is>
2014-01-18 17:51:56 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Websurvey::Application.config.secret_key_base = '2c3233de219389a088d49ab54355627bd4951e431400fe45a89d62bf6801d824cc368124986596060a697145c26097eae5a6a182250f84146b20ce8df421c82c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Websurvey::Application.config.session_store :cookie_store, key: '_websurvey_session'
